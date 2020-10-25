b96cab1713aa51d4b6b524a17c2225752d2fa9e3
jimao <1982775886@qq.com>
2020-10-25 23:19:55 +0800

    提交

--- src/components/fields/index.vue
+++ src/components/fields/index.vue
@@ -3,18 +3,69 @@
     <a-table
       bordered
       size="small"
-      :columns="autoColumns">
+      :pagination="false"
+      :showHeader="showHeader"
+      :columns="autoColumns"
+      :dataSource="autoList">
+      <p slot="expandedRowRender" slot-scope="record">
+        <Field :showHeader="false" :field="record.struct" />
+      </p>
     </a-table>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { IJsObj } from './IJsObj';
+import testJson from './test.json';
+import { StructType } from './type';
 
 @Component({
-
+  name: 'Field',
 })
-export default class XXX extends Vue {
+export default class Field extends Vue {
+  @Prop({ default() { return testJson; }}) private readonly field!: IJsObj;
+  @Prop({ default: true }) private readonly showHeader!: boolean;
+
+
+  public get autoListObject() {
+    return this.field.fields?.map(([name, struct]) => ({
+      name,
+      type: struct.type.toLowerCase(),
+      struct,
+    }));
+  }
+
+  public get autoListArray() {
+    return this.field.element ? [this.field.element] : [];
+  }
+
+  public get autoListTuple() {
+    return this.field.elements?.map((struct, index) => ({
+      name: (index + 1).toString(),
+      type: struct.type.toLowerCase(),
+      struct,
+    }));
+  }
+
+  public get autoListUnoin() {
+    return this.field.members?.map((struct, index) => ({
+      name: (index + 1).toString(),
+      type: struct.type.toLowerCase(),
+      struct,
+    }));
+  }
+
+  public get autoList() {
+    switch (this.field.type) {
+      case StructType.Object: return this.autoListObject;
+      case StructType.Array: return this.autoListArray;
+      case StructType.Tuple: return this.autoListTuple;
+      case StructType.Undefined: return this.autoListUnoin;
+      default: return [];
+    }
+  }
+
   public get autoColumns() {
     return [
       {
@@ -26,6 +77,13 @@ export default class XXX extends Vue {
         title: '类型',
         key: 'type',
         dataIndex: 'type',
+      },
+      {
+        title: '操作',
+        key: 'opts',
+        dataIndex: 'opts',
+        align: 'center',
+        width: 80,
       },
     ];
   }
---
+++ src/components/fields/type.ts
@@ -0,0 +1,49 @@
+/**
+ * Struct类型
+ */
+export enum StructType {
+  /**
+   * Struct Undefined类型
+   */
+  Undefined = 'Undefined',
+  /**
+   * Struct Null类型
+   */
+  Null = 'Null',
+  /**
+   * Struct Boolean类型
+   */
+  Boolean = 'Boolean',
+  /**
+   * Struct Number类型
+   */
+  Number = 'Number',
+  /**
+   * Struct String类型
+   */
+  String = 'String',
+  /**
+   * Struct Date类型
+   */
+  Date = 'Date',
+  /**
+   * Struct 对象类型
+   */
+  Object = 'Object',
+  /**
+   * Struct 数组类型
+   */
+  Array = 'Array',
+  /**
+   * Struct 元组类型
+   */
+  Tuple = 'Tuple',
+  /**
+   * Struct 联合类型
+   */
+  Union = 'Union',
+  /**
+   * Struct 未知的类型
+   */
+  Unknow = 'Unknow',
+}
